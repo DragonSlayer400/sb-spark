e3474f04c73407343121a27b78e9de8c8802dccb
Denis Nurdinov <nurdinovdenis305@gmail.com>
2020-09-29 20:43:50 +0300

    chages lab04a

--- lab04a/filter/src/main/scala/filter.scala
+++ lab04a/filter/src/main/scala/filter.scala
@@ -7,6 +7,9 @@ object filter {
     val spark = SparkSession.builder().getOrCreate()
     import spark.implicits._
     val df = spark.read.format("kafka").option("kafka.bootstrap.servers", "spark-master-1:6667").option("subscribe", "lab04_input_data").load()
+
+    df.show(10)
+
     val json: Dataset[String] = df.select(col("value").cast("string")).as[String]
 
     val getData = udf { (timestamp: Long) =>
@@ -15,8 +18,8 @@ object filter {
     }
     val parsed = spark.read.json(json).select('category,'event_type,'item_id,'item_price,'timestamp, 'uid, getData('timestamp).as("date"))
 
-    parsed.filter(col("event_type") === "view").write.partitionBy("date").mode("overwrite").json("/user/denis.nurdinov/visits/view/")
-    parsed.filter(col("event_type") === "buy").write.partitionBy("date").mode("overwrite").json("/user/denis.nurdinov/visits/buy/")
+    parsed.filter(col("event_type") === "view").write.partitionBy("date").mode("overwrite").json("/view/")
+    parsed.filter(col("event_type") === "buy").write.partitionBy("date").mode("overwrite").json("/buy/")
 
     spark.stop()
 
