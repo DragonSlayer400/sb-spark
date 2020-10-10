9160c1787b2496ba271d0cf7680e689a3a85ea43
Denis Nurdinov <nurdinovdenis305@gmail.com>
2020-10-10 19:21:51 +0300

    add lab05

--- lab05/users_items/src/main/scala/ru/apache/denis/VisitsProcessing.scala
+++ lab05/users_items/src/main/scala/ru/apache/denis/VisitsProcessing.scala
@@ -1,5 +1,7 @@
 package ru.apache.denis
 
+import java.text.SimpleDateFormat
+
 import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.StructType
@@ -12,18 +14,41 @@ import org.apache.spark.sql.sources.In
 class VisitsProcessing(spark: SparkSession, inputDir: String, outputDir: String){
 
   def update_off() ={
-    saveResult(readNew(),findMax())
+    saveResult(readNew(),maxDate())
   }
 
   def update_on() = {
-    val dfNew = readNew()
-    val dfOld = spark.read.parquet(s"$outputDir/*")
-    val unDF = dfNew.union(dfOld).groupBy(col("uid")).sum()
-    val renamedColumn = unDF.columns.map(name => col(name).as(name.replaceAll("^sum\\(","").replaceAll("\\)$","")))
-    val unDFAll = unDF.select(renamedColumn: _* )
-    saveResult(unDFAll, findMax())
+    val buy = udf { (category: String) =>
+      "buy_" + category.replaceAll(" ", "_").replaceAll("-", "_").toLowerCase
+    }
+    val view = udf { (category: String) =>
+      "view_" + category.replaceAll(" ", "_").replaceAll("-", "_").toLowerCase
+    }
+    val dfBuy = spark.read.format("json").option("path", s"$inputDir/buy").load.na.drop("any").select(col("uid"),buy(col("item_id")).as("category"))
+    val dfView = spark.read.format("json").option("path", s"$inputDir/view").load.na.drop("any").select(col("uid"),view(col("item_id")).as("category"))
+    val result = dfBuy.union(dfView).union(readOld()).groupBy(col("uid")).pivot("category").count().na.fill(0)
+    saveResult(result, maxDate())
   }
 
+  def readOld()= {
+    val dfOld = spark.read.parquet(s"$outputDir/*")
+    val nm = dfOld.columns.toList.filter(_ != "uid")
+    val nm_dbl = nm.map(s => "\"" + s + "\", " + s).mkString(", ")
+    val s = "stack(" + nm.length.toString + ", " + nm_dbl + ") as (category, value)"
+    val old_users_items = dfOld.selectExpr("uid", s)
+    old_users_items.select(col("uid"),col("category"))
+  }
+
+  def maxDate() = {
+    val dfBuy = spark.read.format("json").option("path", s"$inputDir/buy").load.na.drop("any")
+    val dfView = spark.read.format("json").option("path", s"$inputDir/view").load.na.drop("any")
+    import spark.implicits._
+    val format = new SimpleDateFormat("yyyyMMdd")
+    format.setTimeZone(java.util.TimeZone.getTimeZone("UTC"))
+    val max_v = dfView.selectExpr("max(timestamp)").as[Long].collect()(0)
+    val max_b = dfBuy.selectExpr("max(timestamp)").as[Long].collect()(0)
+    format.format(if (max_v > max_b) max_v else max_b)
+  }
 
   def readNew() = {
     val buy = udf { (category: String) =>
@@ -39,16 +64,9 @@ class VisitsProcessing(spark: SparkSession, inputDir: String, outputDir: String)
     formattedDfView.join(formattedDfBuy, Seq("uid"),"left").na.fill(0)
   }
 
-  def findMax(): Int = {
-    import spark.implicits._
-    val dfBuy = spark.read.format("json").option("path", s"$inputDir/buy").load.na.drop("any")
-    val dfView = spark.read.format("json").option("path", s"$inputDir/view").load.na.drop("any")
-    val max_v = dfView.selectExpr("max(date_rep)").as[Int].collect()(0)
-    val max_b = dfBuy.selectExpr("max(date_rep)").as[Int].collect()(0)
-    if(max_b > max_v) max_b else max_v
-  }
 
-  def saveResult(df: DataFrame, maxDate: Int) = {
+
+  def saveResult(df: DataFrame, maxDate: String) = {
     df.write.mode("overwrite").parquet(s"$outputDir/$maxDate")
   }
 
